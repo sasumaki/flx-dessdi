4a002312c17923cf3938e55c39757b3fc2be1d8f
sasumaki <saus.makinen@gmail.com>
2020-11-23 15:15:42 +0200

    vitun jeesus

--- meme-detector/app.py
+++ meme-detector/app.py
@@ -8,17 +8,18 @@ app = Flask(__name__)
 # create an endpoint at http://localhost:/8080/
 @app.route("/", methods=["POST"])
 def home():
-    # create a CloudEvent
-    event = from_http(request.headers, request.get_data())
+  print("wat")
+  # create a CloudEvent
+  event = from_http(request.headers, request.get_data())
 
-    # you can access cloudevent fields as seen below
-    print(
-        f"Found {event['id']} from {event['source']} with type "
-        f"{event['type']} and specversion {event['specversion']}"
-    )
+  # you can access cloudevent fields as seen below
+  print(
+      f"Found {event['id']} from {event['source']} with type "
+      f"{event['type']} and specversion {event['specversion']}"
+  )
 
-    return "", 204
+  return "", 204
 
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(host='0.0.0.0', port=8080)
